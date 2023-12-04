b0fb9073d0600fb979b4b633e83d4d8554b5095f
fsampson30 <fsampson30@gmail.com>
2023-12-04 07:46:08 -0300

    Initial commit

---
+++ src/main/java/com/sampson/imersaospringdio/ImersaoSpringDioApplication.java
@@ -0,0 +1,13 @@
+package com.sampson.imersaospringdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImersaoSpringDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImersaoSpringDioApplication.class, args);
+	}
+
+}
